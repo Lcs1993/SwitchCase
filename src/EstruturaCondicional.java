8db1a1d6de5abb1141fc634ba1852f358c1eb5ed
Lcs1993 <ldacosta461@gmail.com>
2021-09-09 09:07:10 -0300

    Switch case

---
+++ src/EstruturaCondicional.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class EstruturaCondicional {
+
+	public static void main(String[] args) {
+		Scanner tec = new Scanner (System.in);
+		System.out.print("Quantas pernas");
+		int perna = tec.nextInt();
+		String tipo;
+		System.out.print("Isso é um(a) ");
+		switch (perna) {
+		case 1:
+				tipo="Saci";
+				break;
+		case 2:
+			tipo= "Bipede";
+			break;
+		case 3:
+			tipo = "tripé";
+			break;
+		case 4:
+			tipo= "Quadrúpede";
+			break;
+		case 6:
+			tipo= "Aranha";
+			break;
+		default:
+			tipo= "ET";
+			break;
+		}
+		System.out.print(tipo);
+
+	}
+
+}
